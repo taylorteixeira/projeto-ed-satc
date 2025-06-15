7730398c503298d899062eff905c671da18588fc
Eduardo Ribarski Scalco <eduardoribarski14@gmail.com>
2025-06-15 11:32:18 -0300

    notbooks e terrsform azure

---
+++ iac/adls/variables.tf
@@ -0,0 +1,8 @@
+# Lista de variaveis utilizadas nos arquivos de terraform
+variable "resource_group_name" {
+  default = "learn-d7beb5bd-e1c5-4b5c-942e-5946e0ca0099"
+}
+
+variable "location" {
+  default = "westus"
+}
