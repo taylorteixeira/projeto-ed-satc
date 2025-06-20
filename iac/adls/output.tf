d08b0e1153f481a39f92b35057df92bd6a550951
ErycMJ <erycmj@gmail.com>
2025-06-19 22:19:50 -0300

    terraform subiu🐲

--- iac/adls/output.tf
+++ iac/adls/output.tf
@@ -1,13 +1,3 @@
-output "adls_name" {
-  description = "Name of the created storage account"
-  value       = azurerm_storage_account.storage.name
-}
-
-output "adls_dfs_endpoint" {
-  description = "Primary DFS endpoint of the created storage account"
-  value       = azurerm_storage_account.storage.primary_dfs_endpoint
-}
-
 output "adls_container_landing_zone" {
   description = "Name of the created storage container landing-zone"
   value       = azurerm_storage_container.landing-zone.name
