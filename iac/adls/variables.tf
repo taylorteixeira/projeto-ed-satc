a9c9a03da27adf92bfc3dbbe35c489aab3f51058
Eduardo Ribarski <133374799+ribarski@users.noreply.github.com>
2025-06-20 21:57:14 -0300

    Update variables.tf

--- iac/adls/variables.tf
+++ iac/adls/variables.tf
@@ -1,6 +1,6 @@
 # Lista de variaveis utilizadas nos arquivos de terraform
 variable "resource_group_name" {
-  default = ""
+  default = "learn-d7beb5bd-e1c5-4b5c-942e-5946e0ca0099"
 }
 
 variable "location" {
